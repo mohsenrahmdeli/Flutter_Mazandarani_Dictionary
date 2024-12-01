9c2e42954ea34a2659aa52e1c8aa3926edcc2eb1
Mohsen <83723914+mohsenrahmdeli@users.noreply.github.com>
2024-12-01 14:41:42 +0400

    Create home.dart

---
+++ home.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'db_Helper.dart';
+import 'models.dart';
+
+class SearchPage extends StatefulWidget {
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  final TextEditingController _searchController = TextEditingController();
+  List<WordModel> _searchResults = [];
+  bool _noResults = false;
+
+  void _searchWords(String query) async {
+    if (query.isEmpty) {
+      setState(() {
+        _searchResults = [];
+        _noResults = false;
+      });
+      return;
+    }
+
+    final results = await DatabaseHelper().searchWords(query);
+    setState(() {
+      _searchResults = results;
+      _noResults = results.isEmpty; // اگر نتایجی وجود ندارد، پرچم تنظیم می‌شود
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Dictionary')),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              textDirection: TextDirection.rtl,
+              controller: _searchController,
+              decoration: const InputDecoration(
+                labelText: 'جستجو',
+                border: OutlineInputBorder(),
+              ),
+              onChanged: _searchWords, // جستجو با هر تغییر در متن
+            ),
+          ),
+          Expanded(
+            child: _noResults
+                ? const Center(child: Text('نتیجه‌ای یافت نشد'))
+                : ListView.separated(
+                    separatorBuilder: (context, index) => const Divider(
+                      color: Colors.grey, // خط جداکننده بین آیتم‌ها
+                      thickness: 1,
+                    ),
+                    itemCount: _searchResults.length,
+                    itemBuilder: (context, index) {
+                      final word = _searchResults[index];
+                      return ListTile(
+                        title: Align(
+                            alignment: Alignment.centerRight,
+                            child: Text(word.word)),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text(
+                              'تلفظ : ${word.read}',
+                              textDirection: TextDirection.rtl,
+                            ),
+                            Text(
+                              'معنی : ${word.mean}',
+                              textDirection: TextDirection.rtl,
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+          ),
+        ],
+      ),
+    );
+  }
+}
