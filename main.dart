acb2a71950c731c7319116cca99e88139f41e255
Mohsen <83723914+mohsenrahmdeli@users.noreply.github.com>
2024-12-01 14:38:58 +0400

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'oldUI/home.dart';
+
+void main() async {
+  runApp(
+      MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SearchPage(),
+    ),
+  );
+}
