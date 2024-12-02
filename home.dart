1f9c66b04b50fdc40d8692240854eaa85a85a230
Mohsen <83723914+mohsenrahmdeli@users.noreply.github.com>
2024-12-02 11:47:40 +0400

    Update home.dart

--- home.dart
+++ home.dart
@@ -13,6 +13,20 @@ class _HomePageState extends State<HomePage> {
   List<WordModel> _searchResults = [];
   bool _noResults = false;
 
+  final TextStyle _titleTextStyle = const TextStyle(
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+    fontFamily: 'Yekan',
+    fontSize: 12,
+  );
+
+  final TextStyle _subtitleTextStyle = const TextStyle(
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+    fontFamily: 'Yekan',
+    fontSize: 18,
+  );
+
   void _searchWords(String query) async {
     if (query.isEmpty) {
       setState(() {
@@ -34,35 +48,7 @@ class _HomePageState extends State<HomePage> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: const Color(0xFFFFEAC5),
-        appBar: AppBar(
-          leading: GestureDetector(
-            onTap: () {
-              SystemNavigator.pop();
-            },
-            child: Padding(
-              padding: const EdgeInsets.only(
-                top: 15,
-                bottom: 15,
-              ),
-              child: Image.asset(
-                'assets/images/exit.png',
-              ),
-            ),
-          ),
-          shadowColor: Colors.redAccent,
-          surfaceTintColor: Colors.redAccent,
-          backgroundColor: const Color(0xFFFFDBB5),
-          title: const Text(
-            'مترجم مازِرونی',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              fontFamily: 'Maryam',
-            ),
-          ),
-          centerTitle: true,
-        ),
+        appBar: _buildAppBar(),
         body: SingleChildScrollView(
           child: ConstrainedBox(
             constraints: BoxConstraints(
@@ -70,137 +56,149 @@ class _HomePageState extends State<HomePage> {
             ),
             child: Column(
               children: [
-                const SizedBox(
-                  height: 15,
-                ),
-                Image.asset(
-                  'assets/images/2.png',
-                  width: 150,
-                  height: 150,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                    textDirection: TextDirection.rtl,
-                    controller: _searchController,
-                    decoration: InputDecoration(
-                      focusColor: Colors.red,
-                      hintText: 'لطفا حرف یا کلمه مورد نظر را بنویسید',
-                      hintTextDirection: TextDirection.rtl,
-                      hintStyle: const TextStyle(
-                        color: Colors.black38,
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(
-                          30.0,
-                        ),
-                      ),
-                    ),
-                    onChanged: _searchWords,
-                  ),
-                ),
-                Expanded(
-                  child: _noResults
-                      ? Center(
-                          child: Column(
-                            children: [
-                              Image.asset(
-                                'assets/images/2.gif',
-                                width: 250,
-                                height: 250,
-                              ),
-                              const Text(
-                                'نتیجه‌ای یافت نشد',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black,
-                                  fontSize: 18,
-                                ),
-                              ),
-                            ],
-                          ),
-                        )
-                      : ListView.separated(
-                          separatorBuilder: (context, index) => const Divider(
-                            indent: 5,
-                            endIndent: 5,
-                            color: Colors.white,
-                            thickness: 1,
-                          ),
-                          itemCount: _searchResults.length,
-                          itemBuilder: (context, index) {
-                            final word = _searchResults[index];
-                            return ListTile(
-                              title: Align(
-                                alignment: Alignment.centerRight,
-                                child: Text(
-                                  'کلمه : ${word.word}',
-                                  style: const TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black,
-                                    fontFamily: 'Yekan',
-                                    fontSize: 12,
-                                  ),
-                                ),
-                              ),
-                              subtitle: Column(
-                                crossAxisAlignment: CrossAxisAlignment.end,
-                                children: [
-                                  SizedBox(
-                                    height: 3.0,
-                                  ),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.end,
-                                    children: [
-                                      Text(
-                                        word.read,
-                                        textDirection: TextDirection.rtl,
-                                        style: const TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.black,
-                                          fontFamily: 'Yekan',
-                                          fontSize: 18,
-                                        ),
-                                      ),
-                                      Text(
-                                        'تلفظ : ',
-                                        textDirection: TextDirection.rtl,
-                                        style: const TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.black,
-                                          fontFamily: 'Yekan',
-                                          fontSize: 12,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  SizedBox(
-                                    height: 3.0,
-                                  ),
-                                  Text(
-                                    'معنی : ${word.mean}',
-                                    textDirection: TextDirection.rtl,
-                                    style: const TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.black,
-                                      fontFamily: 'Yekan',
-                                      fontSize: 12,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            );
-                          },
-                        ),
-                ),
+                const SizedBox(height: 15),
+                _buildLogo(),
+                _buildSearchBar(),
+                Expanded(child: _buildResults()),
               ],
             ),
           ),
         ),
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      leading: GestureDetector(
+        onTap: () => SystemNavigator.pop(),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 15),
+          child: Image.asset('assets/images/exit.png',),
+        ),
+      ),
+      shadowColor: Colors.redAccent,
+      backgroundColor: const Color(0xFFFFDBB5),
+      title: const Text(
+        'مترجم مازِرونی',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+          fontFamily: 'Maryam',
+        ),
+      ),
+      centerTitle: true,
+    );
+  }
+
+  Widget _buildLogo() {
+    return Image.asset(
+      'assets/images/2.png',
+      width: 150,
+      height: 150,
+    );
+  }
+
+  Widget _buildSearchBar() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+          color: Colors.black,
+        ),
+        textDirection: TextDirection.rtl,
+        controller: _searchController,
+        decoration: InputDecoration(
+          hintText: 'لطفا حرف یا کلمه مورد نظر را بنویسید',
+          hintTextDirection: TextDirection.rtl,
+          hintStyle: const TextStyle(color: Colors.black38),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+        ),
+        onChanged: _searchWords,
+      ),
+    );
+  }
+
+  Widget _buildResults() {
+    if (_noResults) {
+      return 
+      Center(
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/images/2.gif',
+              width: 250,
+              height: 250,
+            ),
+            const Text(
+              'نتیجه‌ای یافت نشد',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                fontSize: 18,
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return ListView.separated(
+      separatorBuilder: (_, __) => const Divider(
+        color: Colors.black45,
+        thickness: 1,
+        indent: 5,
+        endIndent: 5,
+      ),
+      itemCount: _searchResults.length,
+      itemBuilder: (context, index) {
+        return _buildWordTile(_searchResults[index]);
+      },
+    );
+  }
+
+  Widget _buildWordTile(WordModel word) {
+    return ListTile(
+      title: Align(
+        alignment: Alignment.centerRight,
+        child: Text(
+          'کلمه : ${word.word}',
+          style: _titleTextStyle,
+        ),
+      ),
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          const SizedBox(
+            height: 3.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                word.read,
+                style: _subtitleTextStyle,
+              ),
+              Text(
+                'تلفظ : ',
+                style: _titleTextStyle,
+                textDirection: TextDirection.rtl,
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 3.0,
+          ),
+          Text(
+            'معنی : ${word.mean}',
+            style: _titleTextStyle,
+            textDirection: TextDirection.rtl,
+          ),
+        ],
       ),
     );
   }
